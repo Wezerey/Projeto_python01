e6af480420e076d221be628a7ad08407660934ec
Wezerey <weslleydiasdecarvalho@hotmail.com>
2024-12-21 13:35:22 -0300

    Adicionado: menu interno

--- Banco.py
+++ Banco.py
@@ -1,24 +1,32 @@
 import random
 
 class ContaCorrente():
-    conta = []
+    contas = {}
     def __init__(self, nome,senha, saldo, conta):
         self._nome = nome
         self._senha = senha
         self._conta = conta
-        self._saldo = saldo
-        ContaCorrente.conta.append(self)
-
+        self.saldo = saldo
+        ContaCorrente.contas[self._conta] = {
+            "nome": self._nome,
+            "senha": self._senha,
+            "saldo": self.saldo
+        }
     def sacar(self, valor):
-        if self._saldo >= valor:
-            self._saldo -= valor
+        if self.saldo >= valor:
+            self.saldo -= valor
             return True
         else:
             print("Saldo insuficiente!")
             return False
+
     def depositar(self, valor):
-        self._saldo += valor
-    def exibir(self, nome,senha,saldo,conta):
-        return f'{nome} {senha}{saldo}{conta}'
+        self.saldo += valor
+    @classmethod
+    def listar_contas(cls):
+        return cls.contas
+    
+    def exibir_saldo(self):
+        return f'Seu saldo atual: {self.saldo:.2f}'
 
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import random
 from Banco import ContaCorrente        
+conta_valida = {}
 def limpar():
     os.system("cls")
 def listar_opcoes():
@@ -32,18 +33,70 @@ def criar_conta():
     conta = random.randint(1000,9999)
     conta_corrente = ContaCorrente(nome, senha, saldo, conta)
     limpar()
-    print(f"Conta criada com sucesso!\nNumero da conta: {conta_corrente._conta}\nSaldo da conta: {conta_corrente._saldo}")
+    print(f"Conta criada com sucesso!\nNumero da conta: {conta_corrente._conta}\nSaldo da conta: {conta_corrente.saldo}")
     voltar_ao_menu()
-    
-    
-    
+def escolher_opção_interna(conta_corrente1):
+    try:
+        escolha = int(input("Digite a opção desejada:"))
+        if escolha == 1:
+            valor = float(input("Digite o valor que deseja sacar: "))
+            if conta_corrente1.sacar(valor):
+                print(f"Saque realizado com sucesso! Saldo atual: R${conta_corrente1.saldo:.2f}")
+            else:
+                print("Não foi possível realizar o saque.")
+        elif escolha == 2:
+            valor = float(input("Digite o valor que deseja depositar: "))
+            conta_corrente1.depositar(valor)
+            print(f"Depósito realizado com sucesso! Saldo atual: R${conta_corrente1.saldo:.2f}")
+        elif escolha == 3:
+            print(ContaCorrente.exibir_saldo(conta_corrente1))
+        elif escolha == 4:
+            voltar_ao_menu()
+        else:
+            menu_interno(conta_corrente1)
+    except:
+        opção_invalida()
+def menu_interno(conta_corrente1):
+    limpar()
+    print("""
+           
+██████╗░███████╗███╗░░░███╗  ██╗░░░██╗██╗███╗░░██╗██████╗░░█████╗░
+██╔══██╗██╔════╝████╗░████║  ██║░░░██║██║████╗░██║██╔══██╗██╔══██╗
+██████╦╝█████╗░░██╔████╔██║  ╚██╗░██╔╝██║██╔██╗██║██║░░██║██║░░██║
+██╔══██╗██╔══╝░░██║╚██╔╝██║  ░╚████╔╝░██║██║╚████║██║░░██║██║░░██║
+██████╦╝███████╗██║░╚═╝░██║  ░░╚██╔╝░░██║██║░╚███║██████╔╝╚█████╔╝
+╚═════╝░╚══════╝╚═╝░░░░░╚═╝  ░░░╚═╝░░░╚═╝╚═╝░░╚══╝╚═════╝░░╚════╝░
+""")
+    print(f"Olá, {conta_corrente1._nome}!")
+    print("1. Sacar")
+    print("2. Depositar")
+    print("3. Exibir saldo")
+    print("4. Sair")
+    escolher_opção_interna(conta_corrente1)
+def entrar():
+    conta_corrente1 = ContaCorrente('Weslley','123',1000,123)
+    try:
+        
+        # conta = int(input("Digite o numero da conta: "))
+        # senha = str(input("Digite a senha: "))
+        conta = 123
+        senha = '123'
+        if conta in ContaCorrente.contas:
+            conta_data = ContaCorrente.contas[conta]
+            if conta_data["senha"] == senha:
+                conta_valida = conta_data           
+                menu_interno(conta_corrente1)
+        voltar_ao_menu()
+    except:
+        print("\nErro: Certifique-se de colocar os dados corretamente")
+        voltar_ao_menu()
 def escolher_opção():
     try:
         escolha = int(input("Digite a opção desejada:"))
         if escolha == 1:
             criar_conta()
         elif escolha == 2:
-            print("entrar")
+            entrar()
         elif escolha == 3:
             print("sair")
         else:
